01cab725dc2774a83edae87b8ab1c0121c2e0b68
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-26 13:09:52 +0300

    add markers

--- mapping.py
+++ mapping.py
@@ -1,3 +1,23 @@
 import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
+import matplotlib.pyplot as plt
+
+#Define coordinates of where we want to center our map
+boulder_coords = [40.015, -105.2705]
+
+#Create the map
+my_map = folium.Map(location = boulder_coords, zoom_start = 13)
+
+#Display the map
+ # add markers
+
+
+CU_Boulder_coords = [40.007153, -105.266930]
+East_Campus_coords = [40.013501, -105.251889]
+SEEC_coords = [40.009837, -105.241905]
+
+#Add markers to the map
+folium.Marker(CU_Boulder_coords, popup = 'CU Boulder').add_to(my_map)
+folium.Marker(East_Campus_coords, popup = 'East Campus').add_to(my_map)
+folium.Marker(SEEC_coords, popup = 'SEEC Building').add_to(my_map)
