51abfc75e461bf2ca1202485a6630fb67ea2578c
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-11 11:50:10 +0300

    view data

--- choropleth_maps.py
+++ choropleth_maps.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 
 shapefile_path = 'data/London_Borough_Excluding_MHW.shp'
 dataset_path = 'data/london-borough-profiles.csv'
+
+#read data
+map_df = gpd.read_file(shapefile_path)
+data_df = pd.read_csv(dataset_path, header=0)
+# view data
+print(f'map dataframe from geopandas{map_df.head()}')
+print(f'dataset entries {data_df.head()}')
+map_df.plot()
+plt.show()
