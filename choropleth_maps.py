8ea7501a9bafa43563baa282941d54d3bc171a4e
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-11 11:46:02 +0300

    load csv and shapefiles

--- choropleth_maps.py
+++ choropleth_maps.py
@@ -1,3 +1,6 @@
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+
+shapefile_path = 'data/London_Borough_Excluding_MHW.shp'
+dataset_path = 'data/london-borough-profiles.csv'
